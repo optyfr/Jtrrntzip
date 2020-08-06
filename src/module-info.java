a75ca5e38dcef71aa42e6a7df4d15b2af0dbc123
opty <opty@NCC-1031>
2020-08-06 09:40:28 +0200

    modularity

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module trrntzip
+{
+	exports JTrrntzip.SupportedFiles;
+	exports JTrrntzip.SupportedFiles.ZipFile;
+	exports JTrrntzip;
+
+	requires org.apache.commons.codec;
+	requires org.apache.commons.io;
+}
