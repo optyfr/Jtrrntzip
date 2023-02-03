67a08d70d256f70aa9628d9726957a0a725c57f5
opty <opty@192.168.111.55>
2023-02-03 12:25:08 +0100

    fix messages package name and release

--- src/jtrrntzip/Messages.java
+++ src/jtrrntzip/Messages.java
@@ -5,7 +5,7 @@ import java.util.ResourceBundle;
 
 public class Messages
 {
-	private static final String BUNDLE_NAME = "JTrrntzip.resources.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "jtrrntzip.resources.messages"; //$NON-NLS-1$
 
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
 
